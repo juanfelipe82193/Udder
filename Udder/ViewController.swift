3dc73a50286271795801c4ce21d005c67efa0a78
Juan Felipe Zorrilla <juan19_93@hotmail.com>
2022-06-10 17:51:50 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Juan Felipe Zorrilla Ocampo on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
